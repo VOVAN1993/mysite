9229db56cf74f5a12b1d49337abf7db7e85678b5
Prikhodko Vladimir <Vladimir.Prikhodko@emc.com>
2015-02-02 03:49:52 +0400

    add comment urls from novajoy project

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,4 +8,23 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^novajoy/', include('novajoy.urls')),
+
+
+
+                       # url(r'^captcha/', include('captcha.urls')),
+                       # url(r'^$',viewCollection),
+                       # url(r'^selectURL/$',viewURL),
+                       # url(r'^addCollection/$',addCollection),
+                       # url(r'^deleteCollection/$',deleteCollection),
+                       # url(r'^addRSS/$',addRSS),
+                       # url(r'^infoAboutCollection/$',infoAboutCollection),
+                       # url(r'^deleteRSS/$',deleteRSS),
+                       # url(r'^accounts/passwordReset/$',resetPassword),
+                       # url(r'^accounts/passwordReset/confirm/(?P<activation_key>\w+)/$',resetPasswordConfirm),
+                       # url(r'^accounts/', include('backends.urls')),
+                       # url(r'^editCollection/$',editCollection),
+                       # url(r'^admin/', include(admin.site.urls)),
+                       # url(r'^about/',about),
+                       # url(r'^contact/',contact),
+                       # url(r'^changedPassword/',changedPassword),
 )
